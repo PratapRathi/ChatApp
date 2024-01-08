d35e5f86480fa5857fa12a001ac9bcf9f85d7f15
Pratap Rathi <prataprathi97@gmail.com>
2024-01-08 20:10:39 +0530

    Added Redux Toolkit setup to this app

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -2,6 +2,7 @@ import React from "react";
 import Chats from "./Chats";
 import { Box, Stack, useTheme } from "@mui/material";
 import Conversation from "../../components/Conversation";
+import Contact from "../../components/Contact";
 
 const GeneralApp = () => {
 
@@ -13,9 +14,11 @@ const GeneralApp = () => {
       <Chats />
 
       {/* Conversation */}
-      <Box sx={{ width: "calc(100vw - 420px)", backgroundColor: theme.palette.mode === "light" ? "#F0F4FA" : theme.palette.background.paper}}>
+      <Box sx={{ width: "calc(100vw - 740px)", backgroundColor: theme.palette.mode === "light" ? "#F0F4FA" : theme.palette.background.paper }}>
         <Conversation />
       </Box>
+      {/* Contact */}
+      <Contact />
     </Stack>
   );
 };
---
+++ src/redux/slices/app.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+//
+import { dispatch } from "../store";
+
+const initialState = {
+    sidebar: {
+        open: false,
+        type: "CONTACT", // can be CONTACT, STARRED, SHARED
+        
+    }
+}
+
+const slice = createSlice({
+    name: 'app',
+    initialState,
+    reducers:{
+        // Toggle Sidebar
+        toggleSidebar(state, action){
+            state.sidebar.open = !state.sidebar.open;
+        },
+        updateSidebarType(state, action){
+            state.sidebar.type = action.payload.type;
+        }
+    }
+})
+
+// Reducer
+export default slice.reducer;
+
+//
