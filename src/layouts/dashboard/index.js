4a72ae77b1b3c2dc89f86ecb651fae5deb57aaef
Pratap Rathi <prataprathi97@gmail.com>
2024-02-06 23:38:23 +0530

    updated window.onload function

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -21,7 +21,7 @@ const DashboardLayout = () => {
     if(isLoggedIn){
       window.onload = function(){
         if(!window.location.hash){
-          window.location = window.location + '#loaded';
+          // window.location = window.location + '#loaded';
           window.location.reload();
         }
       }
